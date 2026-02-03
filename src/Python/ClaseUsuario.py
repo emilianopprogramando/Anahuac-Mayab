7bffea32895ba13b84e725cdde48b07e99b1fc30
emilianopprogramando <penicheemiliano8272@gmail.com>
2026-02-03 10:49:08 -0600

    Se agrega setter y getter para Python

--- src/Python/ClaseUsuario.py
+++ src/Python/ClaseUsuario.py
@@ -6,6 +6,33 @@ class Usuario:
         self.apellidos = apellidos
         self.edad = edad
 
+    # Declaración de setter y getter
+    # Python exige que se programe los getter con @property
+    @property
+    def nombre(self): #get de __nombre
+        return self.__nombre
+
+    @nombre.setter
+    def nombre(self, nombre): #set de __nombre
+        self.__nombre = nombre
+
+    @property
+    def apellidos(self): #get de __apellidos
+        return self.__apellidos
+
+    @apellidos.setter
+    def apellidos(self, apellidos): #set de __apellidos
+        self.__apellidos = apellidos
+
+    @property
+    def edad(self): #get de __edad
+        return self.__edad
+
+    @edad.setter
+    def edad(self, edad): #set de __edad
+        self.__edad = edad
+
+    # Métodos
     def iniciarSesion(self):
         print("El usuario ", self.nombre, " esta iniciando sesión.")
 
--- src/Python/main.py
+++ src/Python/main.py
@@ -6,9 +6,9 @@ maestra = Usuario()
 alumno = Usuario("Tom", "Cruise", 64)
 
 # Asignar valores a los atributos
-# maestra.nombre = "Lizbeth"
-# maestra.apellidos = "Hernandez Olan"
-# maestra.edad = 42
+maestra.nombre = "Lizbeth"
+maestra.apellidos = "Hernandez Olan"
+maestra.edad = 42
 
 # Llamada (invocación) de métodos
 maestra.iniciarSesion()
