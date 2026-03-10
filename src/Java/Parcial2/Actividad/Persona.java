93953a350cb0aa224f38479ac5cb48a149efee31
emilianopprogramando <penicheemiliano8272@gmail.com>
2026-03-10 10:52:09 -0600

    Se agrega la clase Persona.java para la actividad P2A1.

---
+++ src/Java/Parcial2/Actividad/Persona.java
@@ -0,0 +1,57 @@
+package Java.Parcial2.Actividad;
+
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private String genero;
+    private int edad;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String apellido, String genero, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.genero = genero;
+        this.edad = edad;
+    }
+
+    public void printPersona() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+        System.out.println("Genero: " + genero);
+        System.out.println("Edad: " + edad);
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+}
